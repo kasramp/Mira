e435b77776b13b2ed2608be31101b2c9053c87d9
kasramp <kasra_mp@yahoo.com>
2024-06-13 00:21:27 +0200

    Resolve merge conflicts

--- src/main/java/com/madadipouya/mira/controller/IndexController.java
+++ src/main/java/com/madadipouya/mira/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.madadipouya.mira.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,12 @@ import static com.madadipouya.mira.util.UrlUtil.getBaseUrl;
 @Controller
 public class IndexController {
 
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maximumFileSize;
+
     @GetMapping("/")
     public String getHome(Model model, @RequestHeader(value = "User-Agent") String userAgent, HttpServletRequest request) {
+        model.addAttribute("maximumFileSize", maximumFileSize);
         if (!StringUtils.contains(userAgent, "curl")) {
             return "index";
         }
